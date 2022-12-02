993af26e24b8cab36726c321f6f2602a74592292
Demian Ferreiro <epidemian@gmail.com>
2022-12-02 19:33:38 -0300

    Use "shape" instead of "choice" for variable names

--- src/day2.rs
+++ src/day2.rs
@@ -5,9 +5,9 @@ pub fn run() {
     let ans1: u32 = input
         .lines()
         .map(|line| {
-            let opponent_choice = parse_shape(line.as_bytes()[0]);
-            let own_choice = parse_shape(line.as_bytes()[2]);
-            own_choice as u32 + get_round_outcome_score(opponent_choice, own_choice)
+            let opponent_shape = parse_shape(line.as_bytes()[0]);
+            let own_shape = parse_shape(line.as_bytes()[2]);
+            own_shape as u32 + get_round_outcome_score(opponent_shape, own_shape)
         })
         .sum();
     println!("{}", ans1);
@@ -15,18 +15,18 @@ pub fn run() {
     let ans2: u32 = input
         .lines()
         .map(|line| {
-            let opponent_choice = parse_shape(line.as_bytes()[0]);
+            let opponent_shape = parse_shape(line.as_bytes()[0]);
             let round_result = match line.as_bytes()[2] {
                 b'X' => LOSE,
                 b'Y' => DRAW,
                 b'Z' => WIN,
                 _ => unreachable!(),
             };
-            let own_choice = *[Rock, Paper, Scissors]
+            let own_shape = *[Rock, Paper, Scissors]
                 .iter()
-                .find(|&&shape| get_round_outcome_score(opponent_choice, shape) == round_result)
+                .find(|&&shape| get_round_outcome_score(opponent_shape, shape) == round_result)
                 .unwrap();
-            own_choice as u32 + round_result
+            own_shape as u32 + round_result
         })
         .sum();
     println!("{}", ans2);
@@ -44,8 +44,8 @@ const LOSE: u32 = 0;
 const DRAW: u32 = 3;
 const WIN: u32 = 6;
 
-fn get_round_outcome_score(opponent_choice: Shape, own_choice: Shape) -> u32 {
-    match (opponent_choice, own_choice) {
+fn get_round_outcome_score(opponent_shape: Shape, own_shape: Shape) -> u32 {
+    match (opponent_shape, own_shape) {
         (Rock, Paper) | (Paper, Scissors) | (Scissors, Rock) => WIN,
         (a, b) if a == b => DRAW,
         _ => LOSE,
