0693bfbebbcc6e9f399b1a040885dee94bf80dd9
Demian Ferreiro <epidemian@gmail.com>
2022-12-20 21:06:33 -0300

    Compute directory sizes more directly when parsing the terminal output

--- src/day7.rs
+++ src/day7.rs
@@ -1,26 +1,18 @@
+use std::collections::HashMap;
+
 pub fn run(input: &str) -> String {
-    let (dirs, files) = parse_fs_from_terminal_output(input);
-    let dir_sizes: Vec<usize> = dirs
-        .iter()
-        .map(|dir_path| {
-            files
-                .iter()
-                .filter(|(file_path, _size)| file_path.starts_with(dir_path))
-                .map(|(_file_path, size)| size)
-                .sum()
-        })
-        .collect();
+    let dir_sizes = calc_directory_sizes_from_terminal_output(input);
 
     let small_dirs_total_size: usize = dir_sizes
-        .iter()
+        .values()
         .filter(|dir_size| **dir_size <= 100_000)
         .sum();
 
-    let unused_space = 70_000_000 - dir_sizes[0]; // first dir is /
+    let unused_space = 70_000_000 - dir_sizes["/"];
     let space_to_free_up = 30_000_000 - unused_space;
 
     let file_to_delete_size = dir_sizes
-        .iter()
+        .values()
         .filter(|size| **size >= space_to_free_up)
         .min()
         .expect("there should be a big-enough directory to free up space for the update");
@@ -28,9 +20,9 @@ pub fn run(input: &str) -> String {
     format!("{small_dirs_total_size} {file_to_delete_size}")
 }
 
-fn parse_fs_from_terminal_output(terminal_output: &str) -> (Vec<String>, Vec<(String, usize)>) {
-    let mut dir_paths: Vec<String> = vec!["/".into()];
-    let mut sizes_by_file_paths: Vec<(String, usize)> = vec![];
+fn calc_directory_sizes_from_terminal_output(terminal_output: &str) -> HashMap<String, usize> {
+    // Keys are full directory paths, like "/foo/bar"
+    let mut dir_sizes: HashMap<String, usize> = HashMap::new();
 
     let mut curr_dir_stack: Vec<&str> = vec![];
     for line in terminal_output.lines() {
@@ -50,21 +42,14 @@ fn parse_fs_from_terminal_output(terminal_output: &str) -> (Vec<String>, Vec<(St
         } else {
             // line is part of `ls` output.
             let dir_or_size = words[0];
-            let name = words[1];
-
-            let full_path = if curr_dir_stack.is_empty() {
-                format!("/{name}")
-            } else {
-                format!("/{}/{}", curr_dir_stack.join("/"), name)
-            };
-
-            if dir_or_size == "dir" {
-                dir_paths.push(format!("{full_path}/"));
-            } else {
+            if dir_or_size != "dir" {
                 let size: usize = dir_or_size.parse().expect("size should be a valid number");
-                sizes_by_file_paths.push((full_path, size));
+                for i in 0..=curr_dir_stack.len() {
+                    let dir_path = format!("/{}", curr_dir_stack[0..i].join("/"));
+                    *dir_sizes.entry(dir_path).or_insert(0) += size;
+                }
             }
         }
     }
-    (dir_paths, sizes_by_file_paths)
+    dir_sizes
 }
