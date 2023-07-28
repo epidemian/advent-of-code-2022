0be58b7f7b197320bc7d98f41303cdd56e1132df
Demian Ferreiro <epidemian@gmail.com>
2023-07-28 16:56:24 -0300

    Optimize day 23 solution
    Use a 2D vector map to check neighbor positions. This brings down the
    runtime of this problem from ~1s to ~150ms :)

--- src/day_23_unstable_diffusion.rs
+++ src/day_23_unstable_diffusion.rs
@@ -1,7 +1,7 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 pub fn run(input: &str) -> String {
-    let mut elves = parse_elves(input);
+    let (mut elves, mut map) = parse_input(input);
 
     let directions = {
         let n = (0, -1);
@@ -19,45 +19,45 @@ pub fn run(input: &str) -> String {
     let mut round = 0;
     loop {
         let mut proposed_moves = HashMap::new();
-        for elf in elves.iter() {
-            let (x, y) = elf;
+        for (elf_index, (x, y)) in elves.iter().enumerate() {
+            let empty_dirs = directions.map(|dirs| {
+                dirs.iter()
+                    .all(|&(dx, dy)| !map[y.wrapping_add_signed(dy)][x.wrapping_add_signed(dx)])
+            });
 
-            let neighbors =
-                directions.map(|dirs| dirs.map(|(dx, dy)| !elves.contains(&(x + dx, y + dy))));
-
-            let all_empty = neighbors.iter().all(|ns| ns.iter().all(|empty| *empty));
-            if all_empty {
+            if empty_dirs.into_iter().all(|x| x) {
                 continue;
             }
 
             for i in 0..4 {
                 let dir_index = (round + i) % 4;
-                let dir_empty = neighbors[dir_index].iter().all(|empty| *empty);
-                if dir_empty {
+                if empty_dirs[dir_index] {
                     let (dx, dy) = directions[dir_index][0];
-                    let dst = (x + dx, y + dy);
+                    let dst = (x.wrapping_add_signed(dx), y.wrapping_add_signed(dy));
                     proposed_moves
                         .entry(dst)
                         // If there's already another elf wanting to move to this destination, don't
                         // consider either of those moves.
                         .and_modify(|m| *m = Err(()))
-                        .or_insert(Ok(*elf));
+                        .or_insert(Ok(elf_index));
                     break;
                 }
             }
         }
 
-        for (dst, proposed_move) in proposed_moves.iter() {
-            if let Ok(elf) = proposed_move {
-                elves.remove(elf);
-                elves.insert(*dst);
+        for (&(x, y), proposed_move) in proposed_moves.iter() {
+            if let Ok(elf_index) = proposed_move {
+                let (elf_x, elf_y) = elves[*elf_index];
+                map[elf_y][elf_x] = false;
+                map[y][x] = true;
+                elves[*elf_index] = (x, y);
             }
         }
 
         round += 1;
 
         if round == 10 {
-            let (mut min_x, mut min_y) = (0, 0);
+            let (mut min_x, mut min_y) = (usize::MAX, usize::MAX);
             let (mut max_x, mut max_y) = (0, 0);
             for &(x, y) in elves.iter() {
                 min_x = min_x.min(x);
@@ -66,7 +66,7 @@ pub fn run(input: &str) -> String {
                 max_y = max_y.max(y);
             }
             let area = (max_x - min_x + 1) * (max_y - min_y + 1);
-            empty_tiles_at_round_10 = area - elves.len() as i32;
+            empty_tiles_at_round_10 = area - elves.len();
         }
 
         if proposed_moves.is_empty() {
@@ -77,31 +77,47 @@ pub fn run(input: &str) -> String {
     format!("{empty_tiles_at_round_10} {round}")
 }
 
-fn parse_elves(input: &str) -> HashSet<(i32, i32)> {
-    input
+type Map = Vec<Vec<bool>>;
+
+fn parse_input(input: &str) -> (Vec<(usize, usize)>, Map) {
+    let input_size = input.lines().count();
+    // Oversize map a good deal so that elves can move around without going into negative indexes.
+    // This 5x map size works fine even for inputs where every tile is an elf.
+    let map_size = input_size * 5;
+    let margin = input_size * 2;
+    let mut map = vec![vec![false; map_size]; map_size];
+
+    let elves: Vec<_> = input
         .lines()
         .enumerate()
         .flat_map(|(y, line)| {
             line.chars()
                 .enumerate()
                 .filter(|(_x, ch)| *ch == '#')
-                .map(move |(x, _ch)| (x as i32, y as i32))
+                .map(move |(x, _v)| (x + margin, y + margin))
         })
-        .collect()
+        .collect();
+
+    for &(x, y) in elves.iter() {
+        map[y][x] = true;
+    }
+
+    (elves, map)
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn test_sample() {
-        assert_eq!(super::run(SAMPLE), "110 20")
+        assert_eq!(super::run(SAMPLE.trim()), "110 20")
     }
-    const SAMPLE: &str = "\
+    const SAMPLE: &str = "
 ....#..
 ..###.#
 #...#.#
 .#...##
 #.###..
 ##.#.##
-.#..#..";
+.#..#..
+";
 }
