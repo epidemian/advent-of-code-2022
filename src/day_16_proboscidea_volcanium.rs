208cc5ed1414c65aa9dfd8e13ac56c39250ba897
Demian Ferreiro <epidemian@gmail.com>
2023-08-23 18:02:27 -0300

    Day 16: use integers instead of tuples for valve IDs
    This allows using more Vecs and less HashMaps as data structures, and
    improves performance slightly.

--- src/day_16_proboscidea_volcanium.rs
+++ src/day_16_proboscidea_volcanium.rs
@@ -6,46 +6,48 @@ use std::mem;
 // Note: this solution was ~stolen from~ heavily inspired by
 // https://old.reddit.com/r/adventofcode/comments/zn6k1l/2022_day_16_solutions/j2xhog7/
 pub fn run(input: &str) -> String {
-    let graph = parse_graph(input);
-    let distances = graph
-        .keys()
-        .map(|id| {
-            let dists = graph
+    let (valves, start_valve_id) = parse_valves(input);
+    let distances = valves
+        .iter()
+        .map(|valve| {
+            valves
                 .iter()
-                .filter(|&(other_id, valve)| other_id != id && valve.flow_rate > 0)
-                .map(|(other_id, _v)| {
-                    let successors = |id: &_| graph[id].connected_valves.iter().cloned();
-                    let d = shortest_path(id, |id| id == other_id, successors).unwrap();
-                    (*other_id, d)
+                .filter(|&other_valve| other_valve.id != valve.id && other_valve.flow_rate > 0)
+                .map(|other_valve| {
+                    let successors = |&id: &ValveId| valves[id].connected_valves.iter().cloned();
+                    let d =
+                        shortest_path(&valve.id, |&id| id == other_valve.id, successors).unwrap();
+                    (other_valve.id, d)
                 })
-                .collect();
-            (*id, dists)
+                .collect()
         })
         .collect();
 
-    let bitmask_indices = graph
+    let mut bit_index = 0;
+    let valve_bitmasks = valves
         .iter()
-        .filter(|(_id, valve)| valve.flow_rate > 0)
-        .enumerate()
-        .map(|(i, (id, _valve))| {
-            assert!(i < BITMASK_BITS, "too many valves to fit on bitmask");
-            (*id, 1 << i)
+        .map(|valve| {
+            if valve.flow_rate == 0 {
+                return 0;
+            }
+            assert!(bit_index < BITMASK_BITS, "too many valves for bitmask size");
+            bit_index += 1;
+            1 << (bit_index - 1)
         })
         .collect();
 
     let ctx = Context {
-        graph,
+        valves,
         distances,
-        bitmask_indices,
+        valve_bitmasks,
     };
 
-    let start_valve = (b'A', b'A');
     let mut part_1_max_pressures = HashMap::default();
-    visit_all_paths(&ctx, start_valve, 30, 0, 0, &mut part_1_max_pressures);
+    visit_all_paths(&ctx, start_valve_id, 30, 0, 0, &mut part_1_max_pressures);
     let part_1_ans = part_1_max_pressures.values().max().unwrap();
 
     let mut part_2_max_pressures = HashMap::default();
-    visit_all_paths(&ctx, start_valve, 26, 0, 0, &mut part_2_max_pressures);
+    visit_all_paths(&ctx, start_valve_id, 26, 0, 0, &mut part_2_max_pressures);
     let part_2_ans = part_2_max_pressures
         .par_iter()
         .flat_map(|(bitmask_1, pressure_1)| {
@@ -60,22 +62,22 @@ pub fn run(input: &str) -> String {
     format!("{part_1_ans} {part_2_ans}")
 }
 
+type ValveId = usize;
+type Bitmask = u16;
+const BITMASK_BITS: usize = mem::size_of::<Bitmask>() * 8;
+
 struct Valve {
+    id: ValveId,
     flow_rate: u64,
     connected_valves: Vec<ValveId>,
 }
 
 // Bunch up some structures to avoid passing too many parameters around.
 struct Context {
-    graph: HashMap<ValveId, Valve>,
-    distances: HashMap<ValveId, HashMap<ValveId, usize>>,
-    bitmask_indices: HashMap<ValveId, Bitmask>,
+    valves: Vec<Valve>,
+    distances: Vec<Vec<(ValveId, usize)>>,
+    valve_bitmasks: Vec<Bitmask>,
 }
-
-// Prefer 2-byte tuples for IDs instead of &str like "AA" to avoid adding lifetime annotations.
-type ValveId = (u8, u8);
-type Bitmask = u16;
-const BITMASK_BITS: usize = mem::size_of::<Bitmask>() * 8;
 
 fn visit_all_paths(
     ctx: &Context,
@@ -92,47 +94,48 @@ fn visit_all_paths(
         *max_val = released_pressure;
     }
 
-    for (other_valve, &dist) in ctx.distances[&current_valve].iter() {
+    for &(other_valve, dist) in ctx.distances[current_valve].iter() {
         if dist as u64 + 1 > remaining_minutes
-            || (ctx.bitmask_indices[other_valve] & open_valves_bitmask) != 0
+            || (ctx.valve_bitmasks[other_valve] & open_valves_bitmask) != 0
         {
             continue;
         }
         let remaining_minutes = remaining_minutes - dist as u64 - 1;
         visit_all_paths(
             ctx,
-            *other_valve,
+            other_valve,
             remaining_minutes,
-            open_valves_bitmask | ctx.bitmask_indices[other_valve],
-            released_pressure + remaining_minutes * ctx.graph[other_valve].flow_rate,
+            open_valves_bitmask | ctx.valve_bitmasks[other_valve],
+            released_pressure + remaining_minutes * ctx.valves[other_valve].flow_rate,
             max_released_pressure,
         )
     }
 }
 
-fn parse_graph(input: &str) -> HashMap<ValveId, Valve> {
-    input
+fn parse_valves(input: &str) -> (Vec<Valve>, ValveId) {
+    let words_by_line: Vec<Vec<_>> = input
         .lines()
         .map(|line| {
-            let words: Vec<_> = line
-                .split([' ', ';', '=', ','])
+            line.split([' ', ';', '=', ','])
                 .filter(|s| !s.is_empty())
-                .collect();
-            let valve_id = parse_valve_id(words[1]);
-            let flow_rate = words[5].parse().expect("invalid number");
-            let connected_valves = words[10..].iter().map(|s| parse_valve_id(s)).collect();
-            let valve = Valve {
-                flow_rate,
-                connected_valves,
-            };
-            (valve_id, valve)
+                .collect()
         })
-        .collect()
-}
-
-fn parse_valve_id(s: &str) -> ValveId {
-    let bytes = s.as_bytes();
-    (bytes[0], bytes[1])
+        .collect();
+    let valve_ids: HashMap<_, _> = words_by_line
+        .iter()
+        .enumerate()
+        .map(|(i, words)| (words[1], i))
+        .collect();
+    let valves = words_by_line
+        .iter()
+        .enumerate()
+        .map(|(id, words)| Valve {
+            id,
+            flow_rate: words[5].parse().expect("invalid number"),
+            connected_valves: words[10..].iter().map(|s| valve_ids[s]).collect(),
+        })
+        .collect();
+    (valves, valve_ids["AA"])
 }
 
 #[cfg(test)]
